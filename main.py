62b72bbd73e30fae5f131cfd758aaf33c5eeddc7
SkariShogun <122278339+SkariShogun@users.noreply.github.com>
2023-02-10 22:35:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDTextButton
+from kivymd.uix.gridlayout import MDGridLayout
 
-class CalculatorApp(MDApp):
+class TestApp (MDApp):
     def build(self):
-        return MDLabel(text = 'Calculator', pos_hint = {'center_x': 1, 'center_y': 0.5})
+        bl = MDGridLayout(pos_hint = {'centr_x':0.5 , 'center_y':0.5} , cols = 3 , rows = 4 , padding = 50 , spacing = 50)
+        lab = MDTextButton(text = '1')
+        lab1 = MDTextButton(text = '2')
+        lab2 = MDTextButton(text = '3')
+        lab3 = MDTextButton(text = '4')
+        lab4 = MDTextButton(text = '5')
+        lab5 = MDTextButton(text = '6')
+        lab6 = MDTextButton(text = '7')
+        lab7 = MDTextButton(text = '8')
+        lab8 = MDTextButton(text = '9')
+        lab9 = MDTextButton(text = '0')
+        bl.add_widget(lab)
+        bl.add_widget(lab1)
+        bl.add_widget(lab2)
+        bl.add_widget(lab3)
+        bl.add_widget(lab4)
+        bl.add_widget(lab5)
+        bl.add_widget(lab6)
+        bl.add_widget(lab7)
+        bl.add_widget(lab8)
+        bl.add_widget(lab9)
+        return bl
 
-CalculatorApp().run()
+if __name__ == '__main__':
+    TestApp().run()
